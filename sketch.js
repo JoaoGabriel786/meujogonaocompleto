2960e6c8b7aff3085dfeb06544bb4c7ef4a20adb
JoaoGabriel786 <127252069+JoaoGabriel786@users.noreply.github.com>
2023-07-26 19:56:00 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+Matter.Bodies;const Constraint = Matter.Constraint;
+var engine,world,backgroungImg
+
+function preload(){
+alface  = loadImage("assets/alface.webp")
+amexa = loadImage ("assets/amexa.png")
+boca = loadImage("assets/boca.png")
+hamburge = loadImage("assets/hamburge.avif")
+cookie = loadImage ("assets/cokkie.webp")
+frago = loadImage ("assets/frago.jpg")
+jaca = loadImage ("assets/jaca.jpg")
+kiwi = loadImage ("assets/kiwi.png")
+pizza =loadImage ("assets/pizza.png")
+bolo = loadImage ("assets/bolo.avif")
+cozinha = loadImage ("assets/cozinha.avif")
+
+}
+
+
+
+function setup () {
+createCanvas  (800,800)
+var alface = createSprite(400,400,30,10)
+}
+
+function Draw  () {}
